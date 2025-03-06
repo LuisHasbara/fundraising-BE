5908878d22bfbe0505e572ef163a9a2eb9df11e9
Ricardo Alegria <ricardo_alegria@siman.com>
2025-03-06 10:19:14 -0600

    set PGUSER variable

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ import Sequelize from "sequelize";
 
 export const sequelize = new Sequelize(
   process.env.PGDATABASE,
-  "postgres",
+  process.env.PGUSER || "postgres",
   process.env.PGPASSWORD,
   {
     host: process.env.PGHOST,
